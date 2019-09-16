a1164d26f95461b4025eff8e8daa6faa703c2067
John O'Connor <john.oconnor@puppet.com>
2019-09-16 18:10:20 +0100

    Disable Windows Update

--- Boltdir/site-modules/winops/files/winops-preinstall.ps1
+++ Boltdir/site-modules/winops/files/winops-preinstall.ps1
@@ -42,6 +42,26 @@ choco install --no-progress --force --yes vscode
 choco install --no-progress --force --yes googlechrome
 choco install --no-progress --force --yes pdk
 
+# Disable IESC
+
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+# Stop Windows Update
+
+$WindowsUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\"
+$AutoUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+
+If(Test-Path -Path $WindowsUpdatePath) {
+    Remove-Item -Path $WindowsUpdatePath -Recurse
+}
+Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 1
+Set-Service wuauserv -StartupType Disabled
+
+
 # Enable IIS as well.
 
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
